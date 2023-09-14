7ffdd84dffae26e42b5199e0c256825991631d42
OAfonin <oafonin@sportmasterlab.net>
2023-09-14 22:10:01 +0300

    Solve CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,13 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: 'CounterButtonApp',
+  data() {
+    return {
+      counter: 0
+    }
+  }
+})
+
+createApp(App).mount('#app')
