23dda75c4bbe5bf2e2f876e6d61d6082c5e20240
OAfonin <oafonin@sportmasterlab.net>
2023-09-14 20:44:55 +0300

    Solve Vue calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,41 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: 'CalcComponent',
+  data() {
+    return {
+      aValue: undefined,
+      bValue: undefined,
+      operator: undefined
+    }
+  },
+
+  computed: {
+    result() {
+      let result = 0
+
+      if (this.aValue === undefined || this.bValue === undefined || !this.operator) { return result }
+
+      switch (this.operator) {
+        case "sum":
+          result = this.aValue + this.bValue
+          break;
+        case "subtract":
+          result = this.aValue - this.bValue
+          break;
+        case "multiply":
+          result = this.aValue * this.bValue
+          break;
+        case "divide":
+          // this place can return Infinity
+          result = this.bValue !== 0 ? this.aValue / this.bValue : result
+          break;
+      }
+
+      return result
+    }
+  }
+})
+
+createApp(App).mount('#app')
