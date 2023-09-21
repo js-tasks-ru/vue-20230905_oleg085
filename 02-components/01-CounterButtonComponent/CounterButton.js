8edb43866231582548d2b5f8e0f975c889b7bf78
OAfonin <oafonin@sportmasterlab.net>
2023-09-21 14:17:27 +0300

    Solve 02_01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,6 +4,20 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр и порождать событие
+  props: {
+    count: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  },
+  emits: ['update:count'],
 
-  template: `<button type="button">1</button>`,
+  template: `
+    <button
+      type="button"
+      @click="$emit('update:count', count + 1)"
+    >
+      {{ count }}
+    </button>`
 });
